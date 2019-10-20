edb2e406c995f83c6620c36e84795a1330f392f0
ShivamKumarJha <jha.shivam3@gmail.com>
2019-10-20 06:48:17 +0530

    helpers: we dont wanna get rate limited!
    Signed-off-by: ShivamKumarJha <jha.shivam3@gmail.com>

--- helpers/dummydt.sh
+++ helpers/dummydt.sh
@@ -1,14 +1,13 @@
 #!/usr/bin/env bash
 
 [[ -z "$1" ]] && exit 1
-echo "Cloning repo"
 android_tools="$(mktemp tgbotXXX -d --tmpdir=/home/$USER)"
 git clone -q https://github.com/ShivamKumarJha/android_tools.git "$android_tools" --depth 1
+export VERBOSE=n
 
 for var in "$@"; do
     mkdir -p "$android_tools/input"
     cd "$android_tools/input"
-    rm -rf $android_tools/input/*
     echo "Downloading ROM"
     if echo ${var} | grep "https://drive.google.com/" && [[ -e "/usr/local/bin/gdrive" ]]; then
         gdrive download "$(echo ${var} | sed "s|https://drive.google.com/||g" | sed "s|/view.*||g" | sed "s|.*id=||g" | sed "s|.*file/d/||g" | sed "s|&export=.*||g" )" || exit 1
@@ -21,7 +20,8 @@ for var in "$@"; do
     UNZIP_DIR=${FILE/.$EXTENSION/}
     echo "Extracting ROM"
     bash "$android_tools/tools/rom_extract.sh" "$android_tools/input/$FILE*"
-    bash "$android_tools/tools/dummy_dt.sh" "$android_tools/dumps/$UNZIP_DIR"
+    echo "Preparing Dummy DT"
+    bash "$android_tools/tools/dummy_dt.sh" "$android_tools/dumps/$UNZIP_DIR" > /dev/null 2>&1
     echo "Pushing to AndroidBlobs GitHub"
     bash "$android_tools/helpers/androidblobs.sh" "$android_tools/dumps/$UNZIP_DIR" > /dev/null 2>&1
     echo "Done"
--- helpers/romextract.sh
+++ helpers/romextract.sh
@@ -1,14 +1,13 @@
 #!/usr/bin/env bash
 
 [[ -z "$1" ]] && exit 1
-echo "Cloning repo"
 android_tools="$(mktemp tgbotXXX -d --tmpdir=/home/$USER)"
 git clone -q https://github.com/ShivamKumarJha/android_tools.git "$android_tools" --depth 1
+export VERBOSE=n
 
 for var in "$@"; do
     mkdir -p "$android_tools/input"
     cd "$android_tools/input"
-    rm -rf $android_tools/input/*
     echo "Downloading ROM"
     if echo ${var} | grep "https://drive.google.com/" && [[ -e "/usr/local/bin/gdrive" ]]; then
         gdrive download "$(echo ${var} | sed "s|https://drive.google.com/||g" | sed "s|/view.*||g" | sed "s|.*id=||g" | sed "s|.*file/d/||g" | sed "s|&export=.*||g" )" || exit 1
