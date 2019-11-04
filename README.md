d7d96bd5ddbd995dbd4733834caf6efabc03503e
ShivamKumarJha <jha.shivam3@gmail.com>
2019-11-05 00:50:36 +0530

    botstart: update message
    Signed-off-by: ShivamKumarJha <jha.shivam3@gmail.com>

--- README.md
+++ README.md
@@ -4,7 +4,46 @@
 **Setup**:  
 `bash setup.sh`  
   
-Put bot token in `config.js`.  
+### **Usage:**
   
-**Run bot**:  
-`node bot.js`  
+/bloblist <Raw URL of all_files.txt>  
+_Generates proprietary-files.txt_
+
+/ddt <OTA URL>  
+_Creates a [Dummy_DT](https://github.com/ShivamKumarJha/Dummy_DT)_
+
+/dump <OTA URL>  
+_Pushes ROM dump to [AndroidDumps](https://github.com/AndroidDumps)_
+
+/dall <OTA URL>  
+_/ddt + /dump._
+
+/gdrive <URL>  
+_Generates direct download link for gdrive._
+
+/caf <TAG OR PLATFORM>  
+_Find latest caf tag._
+
+/deviceinfos <codename>  
+_Show device information._
+
+/specs <codename>  
+_Shows device specs._
+
+/codename <brand device>  
+_Shows device specs._
+
+/gapps <codename>  
+_Various latest gapps links._
+
+/oneplus <codename>  
+_OnePlus OTA links._
+
+/oppo  
+_Oppo OTA links._
+
+/realme  
+_Realme OTA links._
+
+/twrp <codename>  
+_TWRP links._
--- handlers/botstart.js
+++ handlers/botstart.js
@@ -4,35 +4,7 @@ const config = require("../config.js")
 
 class BotStartController extends TelegramBaseController {
     botstart($) {
-        $.sendMessage(
-            "/bloblist <Raw URL of allfiles.txt>\n" +
-            "_Generates proprietary-files.txt_" +
-            "\n\n/ddt <OTA URL>\n" +
-            "_Creates a _[Dummy_DT](https://github.com/ShivamKumarJha/Dummy_DT)" +
-            "\n\n/dall <OTA URL>\n" +
-            "_/ddt + /dump._" +
-            "\n\n/dump <OTA URL>\n" +
-            "_Pushes ROM dump to _[AndroidDumps](https://github.com/AndroidDumps)" +
-            "\n\n/gdrive <URL>\n" +
-            "_Generates direct download link for gdrive._" +
-            "\n\n/caf <TAG OR PLATFORM>\n" +
-            "_Find latest caf tag._" +
-            "\n\n/deviceinfos <codename>\n" +
-            "_Show device information._" +
-            "\n\n/specs <codename>\n" +
-            "_Shows device specs._" +
-            "\n\n/codename <brand device>\n" +
-            "_Shows device specs._" +
-            "\n\n/gapps <codename>\n" +
-            "_Various latest gapps links._" +
-            "\n\n/oneplus <codename>\n" +
-            "_OnePlus OTA links._" +
-            "\n\n/oppo\n" +
-            "_Oppo OTA links._" +
-            "\n\n/realme\n" +
-            "_Realme OTA links._" +
-            "\n\n/twrp <codename>\n" +
-            "_TWRP links._" , {
+        $.sendMessage("[README](https://github.com/ShivamKumarJha/atbot/blob/master/README.md)" , {
             parse_mode: "markdown",
             disable_web_page_preview: "True",
             reply_to_message_id: $.message.messageId
