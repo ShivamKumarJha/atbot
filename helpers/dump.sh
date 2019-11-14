d13af0575566f633478f7718fa2b7352b861512c
ShivamKumarJha <jha.shivam3@gmail.com>
2019-11-14 10:33:18 +0530

    dump.sh: make it better
    Signed-off-by: ShivamKumarJha <jha.shivam3@gmail.com>

--- helpers/dump.sh
+++ helpers/dump.sh
@@ -11,11 +11,11 @@ for var in "$@"; do
     echo "Downloading ROM"
     if echo ${var} | grep "https://drive.google.com/" && [[ ! -z "$(which gdrive)" ]]; then
         FILE_ID="$(echo "${var:?}" | sed -Er -e 's/https.*id=(.*)/\1/' -e 's/https.*\/d\/(.*)\/(view|edit)/\1/' -e 's/(.*)(&|\?).*/\1/')"
-        gdrive download "$FILE_ID" || exit 1
+        gdrive download "$FILE_ID" || { echo "Download failed!"; rm -rf "$android_tools/"; exit 1; }
     elif echo ${var} | grep "https://mega.nz/" && [[ -e "/usr/bin/megadl" ]]; then
-        megadl "${var}" --no-progress || exit 1
+        megadl "${var}" --no-progress || { echo "Download failed!"; rm -rf "$android_tools/"; exit 1; }
     else
-        aria2c -q -s 16 -x 16 ${var} -d "$android_tools/input" -o "otafile" || { echo "Download failed!"; exit 1; }
+        aria2c -q -s 16 -x 16 ${var} -d "$android_tools/input" -o "otafile" || { echo "Download failed!"; rm -rf "$android_tools/"; exit 1; }
     fi
     find -name "* *" -type f | rename 's/ /_/g'
     URL=$( ls "$android_tools/input/" )
@@ -24,8 +24,12 @@ for var in "$@"; do
     UNZIP_DIR=${FILE/.$EXTENSION/}
     echo "Extracting ROM"
     bash "$android_tools/tools/rom_extract.sh" "$android_tools/input/$URL"
-    bash "$android_tools/helpers/dumpyara_push.sh" "$android_tools/dumps/$UNZIP_DIR"
-    bash "$android_tools/tools/dummy_dt.sh" "$android_tools/dumps/$UNZIP_DIR"
+    if [[ -d "$android_tools/dumps/$UNZIP_DIR" ]]; then
+        bash "$android_tools/helpers/dumpyara_push.sh" "$android_tools/dumps/$UNZIP_DIR"
+        bash "$android_tools/tools/dummy_dt.sh" "$android_tools/dumps/$UNZIP_DIR"
+    else
+        echo "Extraction failed!"
+    fi
     rm -rf "$android_tools/input/$URL"
 done
 rm -rf "$android_tools/"
