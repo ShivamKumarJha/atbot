b47e89e6e434f96d04a6e3e27fdb68cff3bb569e
ShivamKumarJha <jha.shivam3@gmail.com>
2019-11-25 18:02:12 +0530

    ddump.sh: support bad sequence of ota.
     fix it using post-build-incremental
    
    Signed-off-by: ShivamKumarJha <jha.shivam3@gmail.com>

--- helpers/ddump.sh
+++ helpers/ddump.sh
@@ -1,6 +1,8 @@
 #!/usr/bin/env bash
 
 [[ -z "$1" ]] && exit 1
+[[ "$( cat $1 | wc -l )" -lt 2 ]] && echo "Give atleast two URL's!" && exit 1
+OTA_LINES=`cat $1`
 android_tools="$(mktemp tgbotXXX -d --tmpdir=/home/$USER)"
 git clone -q https://github.com/ShivamKumarJha/android_tools.git "$android_tools" --depth 1
 export DUMMYDT=y
@@ -9,13 +11,15 @@ export VERBOSE=n
 
 mkdir -p "$android_tools/input"
 cd "$android_tools/input"
-
-[[ "$( cat $1 | wc -l )" -lt 2 ]] && echo "Give atleast two URL's!" && rm -rf "$android_tools" && exit 1
-OTA_LINES=`cat $1`
 for OTA_LINK in $OTA_LINES; do
     ((OTA_NO++))
-    echo "Downloading OTA Number $OTA_NO"
+    echo "Downloading OTA $OTA_NO"
     aria2c -q -s 16 -x 16 ${OTA_LINK} -d "$android_tools/input" -o ${OTA_NO} || { echo "Download failed!"; rm -rf "$android_tools/"; exit 1; }
+    7z x ${OTA_NO} -y META-INF > /dev/null 2>&1
+    OTAFP="$( find "$android_tools/input" -name "metadata" -exec cat {} \; | grep -iE "post-build=" | sed "s|.*=||1" )"
+    OTAIN="$( find "$android_tools/input" -name "metadata" -exec cat {} \; | grep -iE "post-build-incremental=" | sed "s|.*=||1" )"
+    echo -e "$OTAFP\n$OTAIN"
+    mv ${OTA_NO} "$OTAIN"
 done
 
 OTA_LIST=($(find $android_tools/input -type f -printf "$android_tools/input/%P\n" | sort -uf))
