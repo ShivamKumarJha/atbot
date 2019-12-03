834ebf21c36aa56fa04d320018c21a5474d9aba2
ShivamKumarJha <jha.shivam3@gmail.com>
2019-12-04 02:41:40 +0530

    dump.sh: cleanup
    Signed-off-by: ShivamKumarJha <jha.shivam3@gmail.com>

--- helpers/dump.sh
+++ helpers/dump.sh
@@ -8,21 +8,6 @@ export DUMPYARA=y
 export VERBOSE=n
 
 for var in "$@"; do
-    mkdir -p "$android_tools/input"
-    cd "$android_tools/input"
-    echo "Downloading ROM"
-    if echo ${var} | grep "https://drive.google.com/" && [[ ! -z "$(which gdrive)" ]]; then
-        FILE_ID="$(echo "${var:?}" | sed -Er -e 's/https.*id=(.*)/\1/' -e 's/https.*\/d\/(.*)\/(view|edit)/\1/' -e 's/(.*)(&|\?).*/\1/')"
-        gdrive download "$FILE_ID" || { echo "Download failed!"; rm -rf "$android_tools/"; exit 1; }
-    elif echo ${var} | grep "https://mega.nz/" && [[ -e "/usr/bin/megadl" ]]; then
-        megadl "${var}" --no-progress || { echo "Download failed!"; rm -rf "$android_tools/"; exit 1; }
-    else
-        aria2c -q -s 16 -x 16 "$(echo ${var} | sed "s|bigota.d.miui.com|hugeota.d.miui.com|g" )" || { echo "Download failed!"; rm -rf "$android_tools/"; exit 1; }
-    fi
-    find -name "* *" -type f | rename 's/ /_/g'
-    URL=$( ls "$android_tools/input/" )
-    echo "Extracting ROM"
-    bash "$android_tools/tools/rom_extract.sh" "$android_tools/input/$URL"
-    rm -rf "$android_tools/input/$URL"
+    bash "$android_tools/tools/rom_extract.sh" "${var}"
 done
 rm -rf "$android_tools/"
